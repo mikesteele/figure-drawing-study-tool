b56f946f61bb7de5ea2165b7e3e77cf37bc82167
Mike Steele <mike@steel.fm>
2020-09-03 19:43:54 -0400

    e

--- src/App.js
+++ src/App.js
@@ -13,49 +13,49 @@ export default function App() {
     poseLength: 1 * SECOND
   });
   const oneMinutePoses = [
-    'https://farm8.staticflickr.com/7428/16403366992_ffb439e0c4_b.jpg', // 3
-    'https://farm8.staticflickr.com/7386/16402532561_4b74ec7273_b.jpg', // 3
-    'https://images.squarespace-cdn.com/content/v1/58cfe9e91b10e37dbd7bc584/1490988462787-66KYGE7HKAJE4CKNNQ1B/ke17ZwdGBToddI8pDm48kEsobnGl9DZrmUYTpufSpDF7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UahE8vZqxbM74EnxPV-ETjy4tp7Y7cQnApgcENgadOSyFK4eMZgYA6RDahiKMr8qgw/mrs._martin_luther_king_jr._1_of_3_new_york_1.jpg?format=1500w', // 4
-    'https://images.squarespace-cdn.com/content/v1/58cfe9e91b10e37dbd7bc584/1490988472473-ZD53ZPHCD6OTFCJ4R5MI/ke17ZwdGBToddI8pDm48kIMbOud1oKoCkfUO6INaGcF7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1URDKK3b0to1Mhy-BcODwFNX3qmz0YNtC7vsbfzdviRmovLLPXkhTEjK9lMSTGM1z-g/pere_couturier_paris_france_1948.jpg?format=1000w', // 4
-    'https://i.pinimg.com/originals/f7/3e/73/f73e73602c8642a88556786957e35002.jpg',
+    'https://images.squarespace-cdn.com/content/v1/58cfe9e91b10e37dbd7bc584/1492106334987-8J4N6FJMSOPNMKDALM5J/ke17ZwdGBToddI8pDm48kFv3LHazbGYnNN3Vi_5sr8Z7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UYLS5D5J7_WHFZltObcjbn9lcjbAp0kam0Iruh3pp3NmA-5z3kre7kcTBLQSsQRGFA/nude_no_18.jpg',
+    'https://www.artic.edu/iiif/2/578e6ba1-7d84-17bd-2132-47d8d36b9a4f/full/1200,/0/default.jpg?w=1200&h=800&fit=crop',
+    'https://images.squarespace-cdn.com/content/v1/58cfe9e91b10e37dbd7bc584/1490993169437-1LCUYN1YAAZSRYA8LUA0/ke17ZwdGBToddI8pDm48kHBaUNS0fNiFRgiTHksHhu97gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UQeuZXIG7TjYe36shGZgAh__IQ3dTkyMTKCk1ku1vH23hDu1IQrvWcV79sMhlbVyNQ/large_woman_looking_down_new_york_1994.jpg',
+    'https://collectionapi.metmuseum.org/api/collection/v1/iiif/285064/605228/restricted',
+    'https://images.squarespace-cdn.com/content/v1/58cfe9e91b10e37dbd7bc584/1490993188850-YY9F1V4DW1CEZH975RKV/ke17ZwdGBToddI8pDm48kI4tu_8ycCi4k4Rf-JEDEC97gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UabjZDsffLhBimJM-Q3J7swnisjKD2oWBwbYXcVDNCPyNKOigyfB8BfAL6B2Stgizg/large_woman_nursel_new_york_1997_platinum.jpg?format=1000w',
   ];
   oneMinutePoses.forEach(p => {
     poses.push({
       src: p,
-      poseLength: MINUTE,
+      poseLength: 1 * MINUTE + (30 * SECOND),
     })
   });
   const twoMinutePoses = [
-    'https://farm8.staticflickr.com/7370/16216645818_a9d44f9c43_b.jpg', // 4
-    'https://www.wexphotovideo.com/globalassets/blog-images/2011/03/miles-davis-by-irving-penn.jpg?width=500',
-    'https://farm8.staticflickr.com/7403/16218372057_090b42ba3a_b.jpg', // star
-    'https://farm9.staticflickr.com/8593/16218371997_89d1b090af_b.jpg', // 3
-    'https://farm8.staticflickr.com/7389/16378310836_9fbfb87fa1_b.jpg', // 3
+    'https://images.squarespace-cdn.com/content/v1/58cfe9e91b10e37dbd7bc584/1490993215455-9S4UODZM032BTLXBF5O8/ke17ZwdGBToddI8pDm48kMP7xigjj5460vMkFpApG-R7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UflvpuMvIvQ6gDF_GpzC02MRjBaVyhKbrInBKp-F4p4xQI1NuyNaxRHMfXcqRneseQ/nudes.jpg?format=2500w',
+    'https://d32dm0rphc51dk.cloudfront.net/ND3cp0ieMCDP9IT-OAcoSg/large.jpg',
+    'https://live.staticflickr.com/1525/26347902455_d2e58f261b_b.jpg',
+    'https://hammer.ucla.edu/sites/default/files/migrated-assets/media/Blog/2016/March/Kate_Lawson/2015.7.1.jpg', // x
+    'https://diotimainthegallery.files.wordpress.com/2016/03/wp_20160308_16_00_28_pro.jpg',
   ];
   twoMinutePoses.forEach(p => {
     poses.push({
       src: p,
-      poseLength: (2 * MINUTE) + (30 * SECOND),
+      poseLength: 3 * MINUTE,
     })
   });
   const fiveMinutePoses = [
-    'https://farm9.staticflickr.com/8612/16402530951_7d0b646f7e_b.jpg', // star
-    'https://farm9.staticflickr.com/8664/15781816794_b2e5f5a129_b.jpg', // 4
+    'https://i.pinimg.com/564x/b8/09/80/b809807004cbbe41367fce793945944a.jpg',
+    'https://i.pinimg.com/originals/cf/42/b6/cf42b6f519a2c34b1feba964b4f71777.jpg',
   ];
   fiveMinutePoses.forEach(p => {
     poses.push({
       src: p,
-      poseLength: 5 * MINUTE,
+      poseLength: 6 * MINUTE,
     })
   });
   const tenMinutePoses = [
-    'https://images.squarespace-cdn.com/content/v1/58cfe9e91b10e37dbd7bc584/1490988416950-0OASMS2GGB87CPTZPO46/ke17ZwdGBToddI8pDm48kBAFfBgHjvgZCAG1Cozm4rp7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UZNzdGtQ5VeqeNIYyo779VlVYb69lrnvLX2DVWDh8VKSkJlaroLwFu49mn6kEA5-HQ/jessye_norman_new_york_1983_platinum.jpg',
-    'https://images.squarespace-cdn.com/content/v1/58cfe9e91b10e37dbd7bc584/1490988403903-8C6BAYB4U0SHS511DJ03/ke17ZwdGBToddI8pDm48kM61joYF9ehM9ZvD8wGz2FF7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UcK7KWGWKSXFzoKowgoIBIiSMdOMILRavYezSuqLilYzKRhZv8lFnZKFI_JfLJSLOQ/james_van_der_zee_new_york_1983.jpg?format=2500w',
+    'https://loeildelaphotographie.com/wp-content/uploads/2019/04/04.jpg',
+    'https://d32dm0rphc51dk.cloudfront.net/dg6QA62eV8X1xsFWWvC3dg/large.jpg',
   ];
   tenMinutePoses.forEach(p => {
     poses.push({
       src: p,
-      poseLength: 10 * MINUTE,
+      poseLength: 12 * MINUTE,
     })
   });
   return (
